c74edfedfcadea2ec2b6f4abce79ca06867549cf
kalil0321 <kalil.bouzigues@gmail.com>
2025-10-11 23:17:39 +0200

    Refactor main.py to use relative import for app and change server port from 8000 to 8080

--- server/main.py
+++ server/main.py
@@ -2,9 +2,9 @@
 Agent Stapply cloud server - Entry point
 """
 
-from api.app import app
+from .api.app import app
 
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8080, log_level="info")
